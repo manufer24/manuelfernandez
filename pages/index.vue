aed178945a89191447487e6eef352fea165ea7d4
manufer24 <manuelferg13@gmail.com>
2024-10-27 15:41:57 -0400

    configure meta tags

--- pages/index.vue
+++ pages/index.vue
@@ -80,7 +80,31 @@ useHead({
   title: lang.value.seo.homepage.title,
   meta: [
     {
+      name: "title",
+      content: lang.value.seo.homepage.title,
+    },
+    {
       name: "description",
+      content: lang.value.seo.homepage.description,
+    },
+    {
+      hid: "og:title",
+      property: "og:title",
+      content: lang.value.seo.homepage.title,
+    },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content: lang.value.seo.homepage.description,
+    },
+    {
+      hid: "twitter:title",
+      property: "twitter:title",
+      content: lang.value.seo.homepage.title,
+    },
+    {
+      hid: "twitter:description",
+      property: "twitter:description",
       content: lang.value.seo.homepage.description,
     },
   ],
@@ -130,131 +154,3 @@ useHead({
   }
 }
 </style>
-<!-- <template>
-  <div>
-    <section class="main-container">
-      <article>
-        <h1 class="main-container__title">
-          <NuxtImg
-            class="title-image"
-            src="/assets/jpg/avatar-img.jpg"
-            alt="Manuel Fernández avatar"
-            title="Manuel Fernández avatar"
-          />
-          <span class="title-text">{{ t("greeting.hello") }} 👋🏻</span>
-        </h1>
-
-        <p>
-          {{ t("greeting.me") }}
-          <b class="bold-color-text">{{ t("greeting.name") }}</b
-          >{{ t("greeting.comma") }}
-          {{ t("greeting.job") }}
-        </p>
-
-        <p>
-          {{ t("greeting.currently") }}
-          <b class="bold-color-text">{{ t("greeting.company") }}</b
-          >{{ t("greeting.comma") }}
-          {{ t("greeting.where") }}
-        </p>
-
-        <h2 class="section-title">{{ t("expertise.title") }}</h2>
-
-        <p>
-          {{ t("expertise.description") }}
-        </p>
-
-        <ul class="main-container__list">
-          <li
-            v-for="{ title, description } in t('expertise.list')"
-            :key="title"
-          >
-            <b>{{ title }}</b>
-            {{ description }}
-          </li>
-        </ul>
-
-        <h2 class="section-title section-title--link">
-          <NuxtLink
-            :to="`/assets/pdf/${variantConfig.siteVariant}/cv-manuel-fernandez.pdf`"
-            target="_blank"
-            download
-            >{{ t("expertise.downloadCV") }}</NuxtLink
-          >
-        </h2>
-      </article>
-
-      <hr />
-
-      <article class="main-container__article">
-        <h2 class="main-container__subtitle">{{ t("projects.title") }}</h2>
-
-        <section>
-          <ul>
-            <li v-for="info in projectsCardData" :key="info.id">
-              <CardsProjectcard :info="info" />
-            </li>
-          </ul>
-        </section>
-      </article>
-    </section>
-  </div>
-</template>
-
-<script lang="ts" setup>
-import { useTranslation } from "~/composables/useTranslation";
-import { variantConfig } from "~/config/variant";
-import { API_DATA } from "~/server/api-data";
-
-const projectsCardData = API_DATA.projectsCardData;
-
-const { t } = useTranslation();
-
-useHead({
-  title: variantConfig.siteName,
-});
-</script>
-
-<style scoped lang="postcss">
-.main-container {
-  @apply container pt-5 md:px-10 lg:max-w-[960px] lg:px-5;
-
-  &__title {
-    @apply my-6 flex items-center justify-start gap-x-3 lg:my-10;
-
-    & .title-text {
-      @apply text-4xl font-bold text-letter dark:text-letter-dark lg:text-6xl;
-    }
-
-    & .title-image {
-      @apply block h-[70px] w-[70px] rounded-full bg-background-light-contrast object-cover dark:bg-background-dark-contrast;
-    }
-  }
-
-  &__subtitle {
-    @apply my-6 text-end text-3xl font-bold text-letter dark:text-letter-dark lg:mb-10 lg:text-[56px];
-  }
-
-  & .section-title {
-    @apply my-6 block text-2xl font-bold text-letter dark:text-letter-dark lg:my-10 lg:text-3xl;
-
-    &--link {
-      @apply block text-link dark:text-link-dark lg:hover:underline;
-    }
-  }
-
-  &__list {
-    @apply mb-6 list-disc space-y-4 pl-5 text-lg text-letter dark:text-letter-dark lg:mb-8 lg:text-xl;
-  }
-
-  & .bold-color-text {
-    @apply text-link dark:text-link-dark;
-  }
-
-  &__article {
-    & .projects-list {
-      @apply space-y-1;
-    }
-  }
-}
-</style> -->
