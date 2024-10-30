056baabbc5c825b46478a3202f340b65cbda89d8
manufer24 <manuelferg13@gmail.com>
2024-10-30 11:42:47 -0400

    clean code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,27 +41,3 @@ export default defineNuxtConfig({
     },
   },
 });
-
-// runtimeConfig: {
-//   public: {
-//       apiOrigin:
-//           process.env.NODE_ENV === 'production' && variantConfig.processEnvironment === 'production'
-//               ? variantConfig.siteApiOrigin[variantConfig.siteVariant]
-//               : 'https://sandbox-api.okdescuentos.com/api',
-//       origin: variantConfig.siteOrigin,
-//       variant: variantConfig.siteVariant as
-//           | 'megadescuentos'
-//           | 'jdescuentos'
-//           | 'okdescontos'
-//           | 'bchollos',
-//       host: variantConfig.siteFinalHost,
-//       siteName: variantConfig.siteName,
-//       mode: process.env.NODE_ENV,
-//       lang: variantConfig.siteLangCode[variantConfig.siteVariant],
-//       sentry_dsn: variantConfig.sentryDns[variantConfig.siteVariant],
-//       sentry_environment: process.env.NODE_ENV,
-//       social_auth_keys: variantConfig.socialAuthKeys[variantConfig.siteVariant],
-//       env: process.env.NODE_ENV,
-//       processEnvironment: variantConfig.processEnvironment,
-//   },
-// },
